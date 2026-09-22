2899a2a8fd84d19bd773b24fc219b1d25c87f5f6
Yandrik Guzmán <yandrik3007@gmail.com>
2026-09-22 13:09:42 +0200

    Delete Strings/ej2_strings.php

--- Strings/ej2_strings.php
+++
@@ -1,9 +0,0 @@
-<HTML>
-<HEAD><TITLE> EJ1 Strings – Conversor de Decimal a Binario </TITLE></HEAD>
-<BODY>
-<?php
-$ip="192.18.16.204";
-printf("IP $ip en binario es como %b.%b.%b.%b",substr($ip,0,3),substr($ip,4,2),substr($ip,7,2),substr($ip,10,3));
-?>
-</BODY>
-</HTML>
